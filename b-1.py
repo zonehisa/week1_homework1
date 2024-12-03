590d1d1b4cb5f0073502c31e657a1f0db2184b66
zonehisa <nijipapazoneokun123@gmail.com>
2024-12-03 10:22:52 +0900

    関数にしました

--- b-1.py
+++ b-1.py
@@ -1,5 +1,9 @@
-for i in range(9):
-    for j in range(9):
-        product = (i+1) * (j+1)
-        print(f"{product}", end=" ")
-    print()
+def multiplication_table():
+    for i in range(9):
+        for j in range(9):
+            product = (i + 1) * (j + 1)
+            print(f"{product}", end=" ")
+        print()
+
+
+multiplication_table()
